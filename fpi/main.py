9dfdba823bb87be1bac86d5ec20432dd2b165a7a
Yozh2 <yozhiknick@mail.ru>
2017-11-12 20:59:45 +0300

    Executing matreader from main

--- fpi/main.py
+++ fpi/main.py
@@ -1,9 +1,12 @@
+#!/usr/local/bin/python3
 """
 fpi - Fixed Point Iteration methods comparison.
 """
 
-if __name__ != '__main__':
-    exit()
+# basic imports
+from sys import argv
+import argparse
+import logging
 
 # import local modules
 import jacobi
@@ -11,5 +14,25 @@ import seidel
 import sor
 import matreader
 
+if __name__ == '__main__':
+
+    def parse_args():
+        """ Parses arguments and returns args object to the main program"""
+        parser = argparse.ArgumentParser()
+        parser.add_argument("PATH", type=str,
+                            help="The PATH to the matrix we want work to.")
+        return parser.parse_args()
+
+
+    # Enable logging
+    LOG = u'{}.log'.format(argv[0])
+    logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s] \
+    %(message)s', level=logging.DEBUG, filename=LOG)
+
+
+    ARGS = parse_args()
+    PATH = ARGS.PATH
+
+
 # Ax = b
-A = matreader.read()
+A = matreader.read(PATH)
