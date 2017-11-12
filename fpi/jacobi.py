aa7f432c1623d4f3d61c6d968f2b7e9901df98eb
Yozh2 <yozhiknick@mail.ru>
2017-11-12 22:27:59 +0300

    jacobi method implementation

--- fpi/jacobi.py
+++ fpi/jacobi.py
@@ -1,39 +1,18 @@
 import numpy as np
 
-ITERATION_LIMIT = 1000
+def jacobi(A, b, eps=10e-10):
+    n = len(A)
+    x = np.zeros_like(b)
 
-# initialize the matrix
-A = np.array([[10., -1., 2., 0.],
-              [-1., 11., -1., 3.],
-              [2., -1., 10., -1.],
-              [0.0, 3., -1., 8.]])
-# initialize the RHS vector
-b = np.array([6., 25., -11., 15.])
+    converge = False
+    while not converge:
+        x_new = np.zeros_like(x)
+        for i in range(A.shape[0]):
+            s1 = np.dot(A[i][ :i], x[:i])
+            s2 = np.dot(A[i][i + 1:], x[i + 1:])
+            x_new[i] = (b[i] - s1 - s2) / A[i][i]
 
-# prints the system
-print("System:")
-for i in range(A.shape[0]):
-    row = ["{}*x{}".format(A[i, j], j + 1) for j in range(A.shape[1])]
-    print(" + ".join(row), "=", b[i])
-print()
+        converge = np.sqrt(sum((x_new[i] - x[i]) ** 2 for i in range(n))) <= eps
+        x = x_new
 
-x = np.zeros_like(b)
-for it_count in range(ITERATION_LIMIT):
-    print("Current solution:", x)
-    x_new = np.zeros_like(x)
-
-    for i in range(A.shape[0]):
-        s1 = np.dot(A[i, :i], x[:i])
-        s2 = np.dot(A[i, i + 1:], x[i + 1:])
-        x_new[i] = (b[i] - s1 - s2) / A[i, i]
-
-    if np.allclose(x, x_new, atol=1e-10, rtol=0.):
-        break
-
-    x = x_new
-
-print("Solution:")
-print(x)
-error = np.dot(A, x) - b
-print("Error:")
-print(error)
+    return x
