601e2720b33078e073e937a35bf775191e62f45b
Yozh2 <yozhiknick@mail.ru>
2017-11-13 16:22:48 +0300

    sor module now accessible

--- fpi/main.py
+++ fpi/main.py
@@ -12,7 +12,7 @@ import numpy as np
 # import local modules
 from jacobi import jacobi
 from seidel import seidel
-#   import sor
+from sor import sor
 import matreader
 import grapher
 
@@ -31,10 +31,10 @@ if __name__ == '__main__':
     def parse_args():
         """ Parses arguments and returns args object to the main program"""
         parser = argparse.ArgumentParser()
+        parser.add_argument("OPT", type=str, nargs='?', default='all',
+                            help="'jacobi', 'seidel' or 'sor' computation only")
         parser.add_argument("PATH", type=str,
                             help="The PATH to the matrix we want work to.")
-        parser.add argument("OPT", type=str, nargs='?', default='all',
-                            help="'jacobi', 'seidel' or 'sor' computation only")
         parser.add_argument("EPS", type=float, nargs='?', default=10e-10,
                             help="epsilon, the discrepancy from the precise solution.")
         return parser.parse_args()
@@ -48,25 +48,22 @@ if __name__ == '__main__':
     # parse arguments
     ARGS = parse_args()
     PATH = ARGS.PATH
-    FUNC = ARGS.FUNC
+    OPT = ARGS.OPT
     EPS = ARGS.EPS
 
     # Ax = b
     # Read A, b
     A, b = matreader.read(PATH)
-
-    func = dict('jacobi':jacobi, 'sor':sor, 'seidel':seidel)
-
     if OPT == 'all':
-        # try jacobi
         x_jacobi = print_solution(jacobi, A, b, EPS)
-
-        # try sor
         x_sor = print_solution(sor, A, b, EPS)
-
-        # try seidel
         x_seidel = print_solution(seidel, A, b, EPS)
-    else:
-        x = print_solution(func[OPT], A, b, EPS)
+
+    elif OPT == 'jacobi':
+        x = print_solution(jacobi, A, b, EPS)
+    elif OPT == 'sor':
+        x = print_solution(sor, A, b, EPS)
+    elif OPT == 'seidel':
+        x = print_solution(seidel, A, b, EPS)
 
     # grapher.makeplot(x_jacobi,0,0)
