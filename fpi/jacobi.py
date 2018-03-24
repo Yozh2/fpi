6ec698c3fafdb9255f78435698bc1ff502219983
Nikolai Gaiduchenko <Gaiduchenko.NE@gmail.com>
2018-03-24 08:54:49 +0300

    Jacobi documentation added. Some commented lines of code removed.

--- fpi/jacobi.py
+++ fpi/jacobi.py
@@ -2,6 +2,21 @@ import numpy as np
 import littlemath as lm
 
 def jacobi(A, b, eps=1e-5):
+    """
+    jacobi(A, b, epsilon) - solve the "Ax = b" equasion using jacobi
+    method with the epsilon accuracy using numpy.arrays for speed
+    improvements.
+
+    Each iteration the residual is counted and appended into the static
+    residuals np.array. Also the number of iterations is in count.
+
+    Returns: x solution (np.array), number of iterations (int),
+    residuals (np.array) for each iteration.
+
+    NOTE: You can stop the computation process in any moment just by
+    pressing Ctrl+C in Terminal or sending SIGINT manually. The
+    intermediate results will be printed then.
+    """
     try:
         print(lm.count_iterations(A, b, eps), 'iterations expected')
 
@@ -16,9 +31,6 @@ def jacobi(A, b, eps=1e-5):
             for i in range(n):
                 s1 = np.dot(A[i][ :i], x[:i])
                 s2 = np.dot(A[i][i + 1:], x[i + 1:])
-
-                # s1 = sum(A[i][j] * x[j] for j in range(i))
-                # s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
                 x_new[i] = (b[i] - s1 - s2) / A[i][i]
 
             # Count residual
